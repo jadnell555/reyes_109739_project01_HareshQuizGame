ba3193622f87fc203b23367171ac09c6be39bca6
Jadnell Reyes <75751633+jadnell555@users.noreply.github.com>
2021-05-29 20:55:40 -0400

    Add files via upload

---
+++ cheatSheet.py
@@ -0,0 +1,11 @@
+answersList = ["1", "blue", "25", "haresh", "3.14",
+               "python", "linus", "0", "20", "2.718"]
+
+questionsList = ["How much is 2^0?", "What color was the font of the instructions?", "How much is 1/4 of 100?",
+                 "What was the first word of the game's title?", "Which are the first 3 digits of PI?",
+                 "What programming language are we learning in class?", "What is the first name of the creator of Linux?",
+                 "What is the result of (1 + 2)(1^2 + 3(1) - 4)?", "What is the least amount of moves to solve a Rubik's Cube?",
+                 "Which are the first 4 digits of e (Euler's Constant)?"]
+
+questionTitleList = ["Question #1:", "Question #2:", "Question #3:", "Question #4:", "Question #5:",
+                    "Question #6:", "Question #7:", "Question #8:", "Question #9:", "Question #10:"]
